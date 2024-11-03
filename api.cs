3f0298cbdadf34889f1cc3b08496de674ce2f4fe
v7e5 <jv0@tuta.io>
2024-11-03 14:35:06 +0530

    replace unicode character

--- api.cs
+++ api.cs
@@ -37,10 +37,8 @@ class Auth: IMiddleware {
 
 class XXX {
   static async Task Main() {
-    cl(
-      String.Join("", collatz([Random.Shared.Next(1, 79)])
-        .Select(e => $"[;38;5;{e % 256};1m\u25a0")) + "[0m"
-    );
+    cl(String.Join("", collatz([Random.Shared.Next(1, 79)])
+      .Select(e => $"[;38;5;{e % 256};1m|")) + "[0m");
 
     var builder = WebApplication.CreateEmptyBuilder(new(){
       WebRootPath = "static"
